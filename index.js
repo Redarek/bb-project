3f57aff22929f356f593c520ed2c15cd689b709d
Radmir <radmirka11@gmail.com>
2022-05-18 01:43:17 +0300

    adding files

--- index.js
+++ index.js
@@ -1,88 +1,35 @@
-const mysql = require('mysql');
-const express = require('express');
-var app = express();
-const bodyparser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser');
+const res = require('express/lib/response')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
 
-app.use(bodyparser.json());
+const PORT = process.env.PORT || 3000
 
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'bbDB',
-    multipleStatements: true
-});
+const app = express()
+const serv =  require('http').Server(app);
 
-const PORT = 3000;
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json()); 
+app.use(express.json())
+app.use("/auth", authRouter)
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connection was successful');
-    else
-        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-});
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/signup.html')
+})
+app.use('/client', express.static(__dirname + '/client'))
 
-app.listen(PORT, () => console.log('Server started on port ' + PORT))
+const start = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://admin:admin@cluster0.kem9r.mongodb.net/bigbonch?retryWrites=true&w=majority`) //подключение БД
+        serv.listen(PORT, () => console.log(`Server started on port ${PORT}`)) //запуск сервера
+        const io = require('socket.io')(serv, {}) //socket io инициализация
+        io.sockets.on('connection', function(socket) {
+            console.log('socket connection')
+        }) 
+    } catch {
+        console.log(e)
+    }
+}
 
-//Get all nfts
-app.get('/nfts', (req, res) => {
-    mysqlConnection.query('SELECT * FROM nfts', (err, rows, fields) => {
-        if (!err)
-            res.send(rows);
-        else
-            console.log(err);
-    })
-});
-
-//Get a NFT
-app.get('/nfts/:id', (req, res) => {
-    mysqlConnection.query('SELECT * FROM nfts WHERE nftID = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
-            res.send(rows);
-        else
-            console.log(err);
-    })
-});
-
-//Delete a NFT
-app.delete('/nfts/:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM nfts WHERE nftID = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
-            res.send('Deleted successfully.');
-        else
-            console.log(err);
-    })
-});
-
-
-//Insert a NFT
-app.post('/nfts', (req, res) => {
-    let nft = req.body;
-    var sql = "SET @nftID = ?;SET @Name = ?;SET @url = ?; \
-    CALL nftAddOrEdit(@nftID,@Name,@url);";
-    mysqlConnection.query(sql, [nft.nftID, nft.Name, nft.url], (err, rows, fields) => {
-        if (!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Inserted NFT id : '+element[0].nftID);
-            });
-        else
-            console.log(err);
-    })  
-});
-
-//Update a NFT
-app.put('/nfts', (req, res) => {
-    let nft = req.body;
-    var sql = "SET @nftID = ?;SET @Name = ?;SET @url = ?; \
-    CALL nftAddOrEdit(@nftID,@Name,@url);";
-    mysqlConnection.query(sql, [nft.nftID, nft.Name, nft.url], (err, rows, fields) => {
-        if (!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Successfully updated NFT id : '+element[0].nftID);
-            });
-        else
-            console.log(err);
-    })  
-});
+start()
